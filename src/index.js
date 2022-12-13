36fd06c25053301b47535fe64f3eddf5bafd8deb
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-13 17:21:49 +0100

    playing with setinterval and  clear interval method

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const observable = new Observable((subscriber) =>{
     subscriber.next('hell word wide')
     subscriber.error()
     
-   
-    
 });
 
 observable.subscribe({
@@ -20,8 +18,33 @@ complete: ()=>{
     console.log('complet')
 },
 error: () =>{
-
 }
-
-
 })
+const observable1 = new Observable((subscriber) =>{
+
+   const id = setInterval(() =>{
+        subscriber.next('data from obseervable')
+        console.log('data from set interval')
+    },1000)
+    subscriber.complete()
+
+    return ()=>{
+        clearInterval(id)
+    }
+})
+console.log('before');
+
+observable1.subscribe({
+    next:(va) =>{
+        console.log(va)
+
+    },
+    complete:()=>{
+        console.log('complet from subscribe')
+
+    },
+    error:()=>{
+
+    }
+})
+console.log('after')
